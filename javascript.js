efd01c8abb290d6df2e0fc207f8e86d060b7a4d5
htsteelp1 <htsteelp1@gmail.com>
2026-04-07 08:14:08 -0400

    Added Basic one round logic
    Logic currently only lets you play one round

--- javascript.js
+++ javascript.js
@@ -1,5 +1,56 @@
 let hScore = 0;
 let cScore = 0;
+let hChoice;
+let cChoice;
+let humanScoreValue = document.querySelector("#humanScore .scoreValue");
+humanScoreValue.textContent = hScore.toString();
+let computerScoreValue = document.querySelector("#computerScore .scoreValue");
+computerScoreValue.textContent = cScore.toString();
+let playButton = document.querySelector("#playRound");
+let controls = document.querySelector("#controls");
+controls.addEventListener("click", clickHandler);
+let rockButton = document.createElement("button");
+rockButton.classList.add("controlButton");
+rockButton.textContent = "Rock";
+let scissorsButton = document.createElement("button");
+scissorsButton.classList.add("controlButton");
+scissorsButton.textContent = "Scissors";
+let paperButton = document.createElement("button");
+paperButton.classList.add("controlButton");
+paperButton.textContent = "Paper";
+
+function addControls() {
+    controls.appendChild(rockButton);
+    controls.appendChild(paperButton);
+    controls.appendChild(scissorsButton);
+}
+function removeControls() {
+    let controlList = document.querySelectorAll("button");
+    controlList.forEach((child) => {
+        controls.removeChild(child);
+    })
+}
+
+function clickHandler(event) {
+    switch(event.target) {
+        case playButton:
+            playRound();
+            break;
+        case rockButton:
+            hChoice = 0;
+            performScore();
+            break;
+        case paperButton:
+            hChoice = 1;
+            performScore();
+            break;
+        case scissorsButton:
+            hChoice = 2;
+            performScore();
+            break;
+    }
+}
+
 function numToRPS(choice) {
     switch(choice) {
         case 0:
@@ -11,22 +62,17 @@ function numToRPS(choice) {
     //     Since im submitting this and this course has not taught arrays for JavaScript, I used a switch statemnt
     }
 }
-function getHumanChoice() {
-    switch(prompt("Rock, Paper, or Scissors").toLowerCase()) {
-        case "rock":
-            return 0;
-        case "paper":
-            return 1;
-        case "scissors":
-            return 2;
-    }
-}
+
 function getComputerChoice() {
     return Math.trunc(Math.random()*9)%3; // Gets a random number between 0-2
 }
 function playRound() {
-    let cChoice = getComputerChoice();
-    let hChoice = getHumanChoice();
+    playButton.remove();
+    addControls();
+    cChoice = getComputerChoice();
+
+}
+function performScore() {
     if (cChoice === hChoice) {
     }
     else if ((cChoice+2-hChoice)%3) { // Math Expression I made for Rock Paper Scisors Winner
@@ -37,10 +83,10 @@ function playRound() {
     }
     console.log("Your Choice: "+numToRPS(hChoice));
     console.log("Computer Choice: "+numToRPS(cChoice));
+    humanScoreValue.textContent = hScore.toString();
+    computerScoreValue.textContent = cScore.toString();
+    removeControls();
 }
-let humanScoreValue = document.querySelector("#humanScore .scoreValue");
-humanScoreValue.textContent = hScore.toString();
-let computerScoreValue = document.querySelector("#computerScore .scoreValue");
-computerScoreValue.textContent = cScore.toString();
+
 
 
